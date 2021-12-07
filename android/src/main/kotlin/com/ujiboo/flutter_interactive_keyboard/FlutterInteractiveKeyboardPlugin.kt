8d16eda02b0469488d330d32055d57fa32e4a04f
Philippe Dupuis <philippe@braver.health>
2021-12-07 16:58:48 -0500

    First version

--- android/src/main/kotlin/com/ujiboo/flutter_interactive_keyboard/FlutterInteractiveKeyboardPlugin.kt
+++ android/src/main/kotlin/com/ujiboo/flutter_interactive_keyboard/FlutterInteractiveKeyboardPlugin.kt
@@ -1,24 +1,32 @@
 package com.ujiboo.flutter_interactive_keyboard
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlutterInteractiveKeyboardPlugin: MethodCallHandler {
+class
+FlutterInteractiveKeyboardPlugin : FlutterPlugin, MethodCallHandler {
 
-  companion object {
-    @JvmStatic
+    private lateinit var channel: MethodChannel
+
     fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "flutter_interactive_keyboard")
+        channel = MethodChannel(registrar.messenger(), "flutter_interactive_keyboard")
+        channel.setMethodCallHandler(FlutterInteractiveKeyboardPlugin())
+    }
+
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        result.notImplemented()
+    }
+
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+      channel = MethodChannel(binding.binaryMessenger, "flutter_interactive_keyboard")
       channel.setMethodCallHandler(FlutterInteractiveKeyboardPlugin())
     }
-  }
 
-  var keyboardHeight = 0.0
-
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    result.notImplemented()
-  }
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 }
